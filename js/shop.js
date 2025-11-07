9212977ad052239318d8a0ac1ec12c4f82fd2204
OzamGameOver <eslamhosny979@gmail.com>
2025-11-07 03:13:12 +0200

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,23 +1,23 @@
 function Vip1() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
 
 function Vip2() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
 
 function Vip3() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
 
 function Vip4() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
 
 function Vip5() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
 
 function Vip6() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://discord.gg/eNB28mqhxz";
 }
